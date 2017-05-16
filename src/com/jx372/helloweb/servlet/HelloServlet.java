dc502696e8ec87385a9761c7b855a2618a0027a5
bit-user <bit-user@bit>
2017-05-16 19:08:06 +0900

    jsp/servlet 기초

---
+++ src/com/jx372/helloweb/servlet/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.jx372.helloweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(
+			HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		PrintWriter out = response.getWriter();
+		out.println( "<h1> hello" + name + "</h1>" );
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);  //어떤 게 들어 오던지 get방식으로 처리하는게 관례처럼 받아들어진다.
+	}
+
+}
